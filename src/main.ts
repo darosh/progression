ada38ee35c751d9f17ad68023a7d10ffb37dbc0e
Jan Forst <ensonador@gmail.com>
2019-10-06 14:35:33 +0200

    keys

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 // import router from './router'
 import vuetify from './plugins/vuetify'
-// @ts-ignore
-import VueHotkey from 'v-hotkey'
 
 Vue.config.productionTip = false
-
-Vue.use(VueHotkey)
 
 new Vue({
   // router,
